213794fd363c2e004c63d1af15ef68a757f05c2a
pablobarcala <pablobarcala4@gmail.com>
2023-12-12 10:49:49 -0300

    agregar funcion para cambiar de lista y cerrar el sidenav

--- src/app/components/sidenav-listas/sidenav-listas.component.ts
+++ src/app/components/sidenav-listas/sidenav-listas.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Lista } from '../../interface/Lista';
 import { LISTAS } from '../../interface/mock-listas';
+import { ListaService } from '../../services/lista.service';
+import { SidenavService } from '../../services/sidenav.service';
 
 @Component({
   selector: 'app-sidenav-listas',
@@ -11,4 +13,14 @@ import { LISTAS } from '../../interface/mock-listas';
 })
 export class SidenavListasComponent {
   @Input() lista: Lista = LISTAS[0]
+
+  constructor(
+    private listaService: ListaService,
+    private sidenavService: SidenavService
+  ){}
+
+  changeLista(lista: Lista) {
+    this.listaService.changeSelected(lista)
+    this.sidenavService.toggleSidenav()
+  }
 }
