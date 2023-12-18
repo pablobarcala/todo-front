89f429659d3b6fb4a63df6a4b2fa51d6471b1a4e
pablobarcala <pablobarcala4@gmail.com>
2023-12-18 00:51:42 -0300

    agregar funcion para ver tareas completadas en dia y favoritos

--- src/app/components/tareas-completadas/tareas-completadas.component.ts
+++ src/app/components/tareas-completadas/tareas-completadas.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { TareaService } from '../../services/tarea.service';
 import { Tarea } from '../../interface/Tarea';
 import { TareaItemComponent } from '../tarea-item/tarea-item.component';
-import { Observable, filter } from 'rxjs';
 import { Lista } from '../../interface/Lista';
 import { LISTAS } from '../../interface/mock-listas';
 import { ListaService } from '../../services/lista.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tareas-completadas',
   standalone: true,
-  imports: [TareaItemComponent],
+  imports: [TareaItemComponent, DatePipe],
+  providers: [DatePipe],
   templateUrl: './tareas-completadas.component.html',
   styleUrl: './tareas-completadas.component.css'
 })
@@ -22,14 +23,27 @@ export class TareasCompletadasComponent implements OnInit {
 
   constructor(
     private tareaService: TareaService,
-    private listaService: ListaService
+    private listaService: ListaService,
+    private datePipe: DatePipe
   ){}
 
   ngOnInit(): void {
     this.listaService.getSelected().subscribe((lista: Lista) => {
       this.listaSelected = lista
+
       this.tareaService.getTareas().subscribe((tareas: Tarea[]) => {
-        this.tareas = tareas.filter(t => t.listas.includes(this.listaSelected))
+        if(this.listaSelected === LISTAS[1]){
+          let hoy: string | null = this.datePipe.transform(new Date(), "yyyy-MM-dd")
+
+          if(hoy){
+            console.log(hoy)
+            this.tareas = tareas.filter(t => t.vencimiento == hoy)
+          }
+        } else if(this.listaSelected == LISTAS[2]){
+          this.tareas = tareas.filter(t => t.favorita)
+        } else {
+          this.tareas = tareas.filter(t => t.listas.includes(this.listaSelected))
+        }
       })
     })
   }
