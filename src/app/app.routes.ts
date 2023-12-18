3cb061bb93a05926766a6fa1f20667005b90576f
pablobarcala <pablobarcala4@gmail.com>
2023-12-18 14:41:37 -0300

    crear componente de configuracion y agregar su router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TareaPaginaComponent } from './components/tarea-pagina/tarea-pagina.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: "full"},
     {path: "home", component: HomeComponent},
-    {path: 'pagina/:id', component: TareaPaginaComponent}
+    {path: 'pagina/:id', component: TareaPaginaComponent},
+    {path: 'configuracion', component: ConfiguracionComponent}
 ];
--- src/app/components/sidenav-configuraciones/sidenav-configuraciones.component.ts
+++ src/app/components/sidenav-configuraciones/sidenav-configuraciones.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav-configuraciones',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidenav-configuraciones.component.html',
   styleUrl: './sidenav-configuraciones.component.css'
 })
