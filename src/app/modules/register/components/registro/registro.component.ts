0b1993a2f852659f82c3c857fb62ed36b6f4c2f4
pablobarcala <pablobarcala4@gmail.com>
2023-12-19 19:31:12 -0300

    agregar router en logout y registro

--- src/app/modules/register/components/registro/registro.component.ts
+++ src/app/modules/register/components/registro/registro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
